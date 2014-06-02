ad2de513c40510349b3b8fe5df04d3ce385314c0
John Friel <friel.john@gmail.com>
2014-06-02 16:02:34 -0400

    added class methods for ActiveRecord queries

--- lib/workflow_status.rb
+++ lib/workflow_status.rb
@@ -4,4 +4,16 @@ module WorkflowStatus
   def workflow_statuses
     [:unpublished, :published, :trashed]
   end
+
+  def unpublished
+    where(workflow_status: 0)
+  end
+  
+  def published
+    where(workflow_status: 1)
+  end
+  
+  def trashed
+    where(workflow_status: 2)
+  end
 end
--- spec/workflow_status_spec.rb
+++ spec/workflow_status_spec.rb
@@ -2,13 +2,40 @@ require 'workflow_status'
 
 describe WorkflowStatus do
 
+  before :each do
+    @klass = Class.new do
+      extend WorkflowStatus
+    end
+  end
+
   it 'adds a class method for returning status names as symbols' do
-    class DummyClass; end
-    DummyClass.extend WorkflowStatus
+    @klass.workflow_statuses.should include(:unpublished) 
+    @klass.workflow_statuses.should include(:published) 
+    @klass.workflow_statuses.should include(:trashed) 
+  end
+
+  # workflow_status 0
+  describe '#unpublished' do
+    it 'sends an ActiveRecord query for the correct workflow_status' do
+      @klass.should_receive(:where).with(workflow_status: 0) 
+      @klass.unpublished
+    end
+  end
   
-    DummyClass.workflow_statuses.should include(:unpublished) 
-    DummyClass.workflow_statuses.should include(:published) 
-    DummyClass.workflow_statuses.should include(:trashed) 
+  # workflow_status 1
+  describe '#published' do
+    it 'sends an ActiveRecord query for the correct workflow_status' do
+      @klass.should_receive(:where).with(workflow_status: 1) 
+      @klass.published
+    end
+  end
+  
+  # workflow_status 2
+  describe '#trashed' do
+    it 'sends an ActiveRecord query for the correct workflow_status' do
+      @klass.should_receive(:where).with(workflow_status: 2) 
+      @klass.trashed
+    end
   end
 
 end
