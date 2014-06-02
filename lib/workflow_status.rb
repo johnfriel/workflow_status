ea6eaba5cd50577ddafb7e26c2e5ef5b298f93e4
John Friel <friel.john@gmail.com>
2014-06-02 16:53:51 -0400

    reduced duplication in class methods

--- lib/workflow_status.rb
+++ lib/workflow_status.rb
@@ -2,6 +2,11 @@ require "workflow_status/version"
 require "workflow_status/instance_methods"
 
 module WorkflowStatus
+  WORKFLOW_STATUS_MAP = {
+    unpublished:  0,
+    published:    1,
+    trashed:      2
+  }
   
   class << self
     def extended(extender)
@@ -10,28 +15,34 @@ module WorkflowStatus
   end
   
   def workflow_statuses
-    [:unpublished, :published, :trashed]
+    workflow_status_map.keys
   end
 
   def unpublished
-    where(workflow_status: 0)
+    where(workflow_status: workflow_status_code(:unpublished))
   end
   
   def published
-    where(workflow_status: 1)
+    where(workflow_status: workflow_status_code(:published))
   end
   
   def trashed
-    where(workflow_status: 2)
+    where(workflow_status: workflow_status_code(:trashed))
   end
 
 
   # for Rails form select helper
   def status_options_for_select
-    [
-      [:unpublished, 0],
-      [:published,   1],
-      [:trashed,     2],
-    ]
+    workflow_status_map.to_a
+  end
+
+  private
+
+  def workflow_status_code(text_value)
+    workflow_status_map.fetch text_value.to_sym
+  end
+
+  def workflow_status_map
+    WORKFLOW_STATUS_MAP
   end
 end
